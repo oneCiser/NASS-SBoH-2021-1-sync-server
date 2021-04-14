66890ec687b2150f024359558aef248268195c13
oneCiser <41345659+oneCiser@users.noreply.github.com>
2021-04-14 09:30:46 -0500

    port

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const { createServer } = require('sync-files-cipher')// require('../syncProyect/
 createServer(
     '8BZ3pCTp71LX5I//QsBYdz7w4JHXNVehSBXuXnScdqg=',
     'AAAAAAAAAAAAAAAAAAAAAA==',
-    () => { console.log("Server listening");}
+    () => { console.log("Server listening");},
+    3030
 )
